96410b4b998d40863bcb2f09b43aca6180b48fa1
whp5047 <whp5047@gmail.com>
2022-11-29 22:42:51 +0900

    #2 Student, Score, Subject로 나눠서 생성

---
+++ src/school/Student.java
@@ -0,0 +1,50 @@
+package school;
+
+import java.util.ArrayList;
+
+public class Student {
+	
+	private int studentId; // 학번
+	private String studentName; // 이름
+	private Subject majorSubject; // 전공과목
+	
+	//학생의 성적 리스트
+	private ArrayList<Score> scoreList = new ArrayList<Score>(); //addSubjectScore() 메서드가 호출되면 리스트에 추가
+
+	public Student(int studentId, String studentName, Subject majorSubject) {
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.majorSubject = majorSubject;
+	}
+	
+	public void addSubjectScore(Score score) {
+		scoreList.add(score);
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public Subject getMajorSubject() {
+		return majorSubject;
+	}
+
+	public void setMajorSubject(Subject majorSubject) {
+		this.majorSubject = majorSubject;
+	}
+	
+	
+	
+}
---
+++ src/school/Subject.java
@@ -0,0 +1,58 @@
+package school;
+
+import java.util.ArrayList;
+
+import utils.Define;
+
+public class Subject {
+	
+	private String subjectName;   // 과목 이름
+	private int subjectId;        // 과목 코드
+	private int gradeType; 		  // 과목 평가 방법 기본 A, B방식
+	
+	//수강신청 학생리스트
+	private ArrayList<Student> studentList = new ArrayList<Student>(); // register() 메서드 호출시 add
+
+	public Subject(String subjectName, int subjectId) {
+		this.subjectName = subjectName;
+		this.subjectId = subjectId;
+		this.gradeType = Define.AB_TYPE; // 기본적으로 A,B타입
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public int getGradeType() {
+		return gradeType;
+	}
+
+	public void setGradeType(int gradeType) {
+		this.gradeType = gradeType;
+	}
+
+	public ArrayList<Student> getStudentList() {
+		return studentList;
+	}
+
+	public void setStudentList(ArrayList<Student> studentList) {
+		this.studentList = studentList;
+	}
+	
+	
+	
+	
+	
+}
